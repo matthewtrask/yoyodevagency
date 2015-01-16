c0b1268f7d7326710ee55b8916ae047b0d0280c5
rocketpastsix <mjftrask@gmail.com>
2015-01-15 22:38:49 -0500

    ehhhh

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <author name="Matt Trask" role="Developer">
         <title>YoYo Dev Studio | Creative Development for your website and business</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
